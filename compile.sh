a37f3be80188eb3415cf4972327abf330a90869a
Stefano Donati <stefano.donati@elettronicagf.it>
2018-04-13 14:39:21 +0200

    Added support for the following SOM modules WID0510_AC01.02 WID0510_AE01.02 WID0510_AF01.02 WID0510_AG01.02 WID0510_AJ01.02 WID0510_AK01.02

--- compile.sh
+++ compile.sh
@@ -45,12 +45,18 @@ fi
 compile_dts 0510_AA01.01 $OUTPUTDIR
 compile_dts 0510_AB01.01 $OUTPUTDIR
 compile_dts 0510_AC01.01 $OUTPUTDIR
+compile_dts 0510_AC01.02 $OUTPUTDIR
 compile_dts 0510_AD01.01 $OUTPUTDIR
 compile_dts 0510_AE01.01 $OUTPUTDIR
+compile_dts 0510_AE01.02 $OUTPUTDIR
 compile_dts 0510_AF01.01 $OUTPUTDIR
+compile_dts 0510_AF01.02 $OUTPUTDIR
 compile_dts 0510_AG01.01 $OUTPUTDIR
+compile_dts 0510_AG01.02 $OUTPUTDIR
 compile_dts 0510_AJ01.01 $OUTPUTDIR
+compile_dts 0510_AJ01.02 $OUTPUTDIR
 compile_dts 0510_AK01.01 $OUTPUTDIR
+compile_dts 0510_AK01.02 $OUTPUTDIR
 
 cp arch/arm/boot/zImage $OUTPUTDIR
 
