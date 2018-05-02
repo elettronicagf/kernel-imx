7804b1ed2ac697df636f16126f5d8885dfdabfa6
Stefano Donati <stefano.donati@elettronicagf.it>
2018-05-02 15:53:21 +0200

    Added support for WID0500_AD01.01 SOM modules

--- compile.sh
+++ compile.sh
@@ -49,6 +49,7 @@ fi
 compile_dts 0500_AA01.01 $OUTPUTDIR
 compile_dts 0500_AB01.01 $OUTPUTDIR
 compile_dts 0500_AC01.01 $OUTPUTDIR
+compile_dts 0500_AD01.01 $OUTPUTDIR
 
 cp arch/arm/boot/zImage $OUTPUTDIR
 
