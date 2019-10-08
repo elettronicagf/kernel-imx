7080738f9f4d1207bd819b7302f787c2d00e9b70
Gianluca Cornacchia <c.luca74@gmail.com>
2019-10-08 15:36:17 +0200

    Fix for wakeup WILC3000
        Wait for clock settle bug
        Change code to match 3mS wakeup delay comment

--- drivers/net/wireless/mchp/wilc_wlan.c
+++ drivers/net/wireless/mchp/wilc_wlan.c
@@ -884,8 +884,8 @@ void chip_wakeup_wilc3000(struct wilc *wilc, int source)
 		 * if still off, wait for another 1ms, for a total wait of 3ms.
 		 * If still off, redo the wake up sequence
 		 */
-		while (((clk_status_reg_val & clk_status_bit) == 0) &&
-		       (((++trials) % 3) == 0)) {
+		while ((clk_status_reg_val & clk_status_bit) == 0 &&
+		       (++trials % 4) != 0) {
 			/* Wait for the chip to stabilize*/
 			usleep_range(1000, 1100);
 
